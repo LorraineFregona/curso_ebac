a5799bf8f603c5ad9dec9a71d788e422d8572d96
Lorraine <lorraineof25@gmail.com>
2025-10-29 12:50:41 -0300

    m26

---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+//Multiplicação
+function multiplicar(num1: number, num2:number): number {
+    return num1 * num2;
+}
+
+//Saudação 
+function saudacao(nome: string): string {
+    return 'Olá ${nome}';
+}
